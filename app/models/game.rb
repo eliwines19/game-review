0efd38ff3c02e69a950e9022ab4286b858589a18
Elijah Wines <wines.elijah@gmail.com>
2022-08-15 21:22:24 -0400

    created methods and requirements of game model

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,2 +1,15 @@
 class Game < ApplicationRecord
+    has_many :reviews
+
+    before_create :slugify
+
+    def slugify
+        self.slug = name.parameterize
+    end
+
+    def avg_score
+        reviews.average(:score).round.to_f
+    end
+
+
 end

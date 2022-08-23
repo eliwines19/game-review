223df1627217c47371caf0b018f48f6163e2e90e
Elijah Wines <wines.elijah@gmail.com>
2022-08-22 20:30:00 -0400

    added yarn and began state and effect of games component

--- app/javascript/components/Games/Games.js
+++ app/javascript/components/Games/Games.js
@@ -1,6 +1,13 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 const Games = () => {
+    const [games, setGames] = useState([])
+
+    useEffect(() => {
+        // get games from api
+        // add games to games state
+    })
+    
     return (
         <div>This is the Games#index view for our app</div>
     )

3dcd77a782f20d64da7b1bc782367c08466c3a51
Elijah Wines <wines.elijah@gmail.com>
2022-08-27 17:39:59 -0400

    Header is showing correct data, beginning styling

--- app/javascript/components/Game/Game.js
+++ app/javascript/components/Game/Game.js
@@ -2,6 +2,12 @@ import React, { useState, useEffect } from 'react'
 import { useParams } from 'react-router-dom'
 import axios from 'axios'
 import Header from './Header'
+import styled from 'styled-components'
+
+const Wrapper = styled.div``
+const Column = styled.div``
+const Main = styled.div``
+
 
 const Game = () => {
     const [game, setGame] = useState({})
@@ -25,11 +31,16 @@ const Game = () => {
     return (
         <div className="wrapper" >
             <div className="column">
-                {
-                    loaded &&
-                    <Header attributes={game.data.attributes}/>
-                }
-                <div className="reviews"></div>
+                <div className="main">
+                    {
+                        loaded &&
+                        <Header
+                            attributes={game.data.attributes}
+                            reviews={game.included}
+                        />
+                    }
+                    <div className="reviews"></div>
+                </div>
             </div>
             <div className="column">
                 <div className="review-form">review form here</div>
--- app/javascript/components/Game/Header.js
+++ app/javascript/components/Game/Header.js
@@ -2,17 +2,21 @@ import React from 'react'
 
 const Header = (props) => {
     const { avg_score, description, image_url, title } = props.attributes
+    const total = props.reviews.length
 
-    console.log(avg_score)
-    console.log(description)
-    console.log(image_url)
-    console.log(title)
+    const displayTotal = () => {
+        if (total == 1) {
+            return <div className="totalReviews">{total} review</div>
+        } else {
+            return <div className="totalReviews">{total} reviews</div>
+        }
+    }
 
     return (
         <div className="wrapper">
-            <h1> <img src={image_url} /> </h1>
+            <h1><img src={image_url} alt={title}/>{title}</h1>
             <div>
-                <div className="totalReviews"></div>
+                {displayTotal()}
                 <div className="starRating"></div>
                 <div className="totalOutOf">{avg_score} out of 5</div>
             </div>

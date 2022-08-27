3007da28ade850b54ba2a6fead13655bfe45621e
Elijah Wines <wines.elijah@gmail.com>
2022-08-27 18:40:14 -0400

    styled game#show and added ReviewForm component

--- app/javascript/components/Game/Header.js
+++ app/javascript/components/Game/Header.js
@@ -1,26 +1,73 @@
 import React from 'react'
+import styled from 'styled-components'
+
+const Wrapper = styled.div`
+    padding: 50px 100px 50px 0;
+`
+const Card = styled.div`
+
+    img {
+        width: 100%;
+        heigth: 100%;
+    }
+
+    h1 {
+        text-align: center;
+        margin-bottom: 0;
+    }
+
+    p {
+        margin-top: 0;
+        padding: 5px;
+        border: solid 1px #000;
+    }
+
+`
+const TotalReviews = styled.div`
+    font-size: 18px;
+    padding: 10px 0;
+`
+const TotalOutOf = styled.div`
+    font-size: 18px;
+    font-weight: bold;
+    padding: 10px 0;
+`
 
 const Header = (props) => {
     const { avg_score, description, image_url, title } = props.attributes
     const total = props.reviews.length
 
-    const displayTotal = () => {
-        if (total == 1) {
-            return <div className="totalReviews">{total} review</div>
+    const displayReviews = () => {
+        if (total == 0) {
+            return <div>No Reviews Yet</div>
+        } else if (total > 1) {
+            return (
+                <div>
+                    <TotalReviews>{total} reviews</TotalReviews>
+                    <TotalOutOf>{avg_score} out of 5</TotalOutOf>
+                </div>
+            )   
         } else {
-            return <div className="totalReviews">{total} reviews</div>
+            return (
+                <div>
+                    <TotalReviews>{total} review</TotalReviews>
+                    <TotalOutOf>{avg_score} out of 5</TotalOutOf>
+                </div>
+            )
         }
     }
 
     return (
-        <div className="wrapper">
-            <h1><img src={image_url} alt={title}/>{title}</h1>
+        <Wrapper>
+            <Card>
+                <h1>{title}</h1>
+                <img src={image_url} alt={title}/>
+                <p>{description}</p>
+            </Card>
             <div>
-                {displayTotal()}
-                <div className="starRating"></div>
-                <div className="totalOutOf">{avg_score} out of 5</div>
+                {displayReviews()}
             </div>
-        </div>
+        </Wrapper>
     )
 }
 

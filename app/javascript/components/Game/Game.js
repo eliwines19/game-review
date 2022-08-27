35b82662a4a96e0e1914722f32be2ef75b065d1e
Elijah Wines <wines.elijah@gmail.com>
2022-08-27 17:48:39 -0400

    styled game#show

--- app/javascript/components/Game/Game.js
+++ app/javascript/components/Game/Game.js
@@ -4,9 +4,24 @@ import axios from 'axios'
 import Header from './Header'
 import styled from 'styled-components'
 
-const Wrapper = styled.div``
-const Column = styled.div``
-const Main = styled.div``
+const Wrapper = styled.div`
+    margin-left: auto;
+    margin-right: auto;
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+`
+const Column = styled.div`
+    background: #fff;
+    height: 100vh;
+    overflow: scroll;
+
+    &:last-child {
+        background: #000;
+    }
+`
+const Main = styled.div`
+    left-padding: 50px;
+`
 
 
 const Game = () => {
@@ -29,9 +44,9 @@ const Game = () => {
     }, [])
 
     return (
-        <div className="wrapper" >
-            <div className="column">
-                <div className="main">
+        <Wrapper>
+            <Column>
+                <Main>
                     {
                         loaded &&
                         <Header
@@ -40,12 +55,12 @@ const Game = () => {
                         />
                     }
                     <div className="reviews"></div>
-                </div>
-            </div>
-            <div className="column">
+                </Main>
+            </Column>
+            <Column>
                 <div className="review-form">review form here</div>
-            </div>
-        </div>
+            </Column>
+        </Wrapper>
     )
 }
 

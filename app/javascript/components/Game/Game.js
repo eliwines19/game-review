0ccb9367cbe033300bafd9ec2c6eb9e4066ced0e
Elijah Wines <wines.elijah@gmail.com>
2022-10-08 16:26:37 -0400

    page successfully loads the newly created review

--- app/javascript/components/Game/Game.js
+++ app/javascript/components/Game/Game.js
@@ -60,7 +60,7 @@ const Game = () => {
         const game_id = game.data.id
         axios.post('/api/v1/reviews.json', {review, game_id})
         .then(resp => {
-            const included = [...game.included, resp.data]
+            const included = [...game.included, resp.data.data]
             setGame({...game, included})
             setReview({title: '', description: '', score: 0})
         })
